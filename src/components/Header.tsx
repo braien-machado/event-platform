a8fb3e74885299c613686e45198892034d612935
Braien Machado de Paula <braienmp@outlook.com>
2022-06-28 10:46:57 -0300

    fix: show menu button in smaller screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,29 @@
+import { List } from 'phosphor-react';
 import React from 'react';
 import Logo from './Logo';
 
-export default function Header() {
+interface HeaderProps {
+  // eslint-disable-next-line no-unused-vars
+  toggleMenu: (bool: boolean) => void;
+  isMenuHidden: boolean;
+}
+
+export default function Header(props: HeaderProps) {
+  const { toggleMenu, isMenuHidden } = props;
+
   return (
-    <header className="w-full p-5 flex justify-center items-center bg-gray-700 border-b border-gray-100">
+    <header
+      className="w-full py-5 px-8 flex justify-between lg:justify-center items-center bg-gray-700 border-b border-gray-100"
+    >
       <Logo />
+      <button
+        onClick={() => toggleMenu(!isMenuHidden)}
+        type="button"
+        className="flex items-center lg:hidden gap-2"
+      >
+        <h1 className="text-xl">Músicas</h1>
+        <List size={40} className="text-blue-500" />
+      </button>
     </header>
   );
 }
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
@@ -7,10 +7,12 @@ import Video from '../components/Video';
 const DEFAULT_SLUG = 'popular-monster';
 
 export default function Event() {
+  const [isMenuHidden, setIsMenuHidden] = useState(true);
+
   const { slug } = useParams<{ slug: string }>();
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Header toggleMenu={setIsMenuHidden} isMenuHidden={isMenuHidden} />
       <main className="flex flex-1">
         <Video songSlug={slug || DEFAULT_SLUG} />
         <Sidebar />
