44cdae997cc826b90c9b9132d9414474f04dfccf
Braien Machado de Paula <braienmp@outlook.com>
2022-06-28 13:43:49 -0300

    style: remove slide animation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,8 +14,9 @@ export default function Sidebar(props: SidebarProps) {
   return (
     <aside
       className={classNames('absolute z-10 lg:static lg:block w-[348px] bg-gray-700 p-6 border-gray-600 transition-all', {
-        '-right-[348px]': isMenuHidden,
+        // '-right-[348px]': isMenuHidden,
         'right-0': !isMenuHidden,
+        hidden: isMenuHidden,
       })}
     >
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
